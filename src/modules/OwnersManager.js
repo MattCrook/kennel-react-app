5bed1c11ed6f4a6eeaf69aca4a0342a433eb44a7
Matt Crook <matt.crook11@gmail.com>
2020-02-23 15:24:49 -0600

    fix: todos- animalcard refactor. Refactor other component cards.

--- src/modules/OwnersManager.js
+++ src/modules/OwnersManager.js
@@ -1,11 +1,11 @@
 const remoteURL = "http://localhost:5002";
 
 export default {
-    getId(id) {
-        return fetch(`${remoteURL}/owners/${id}`).then(resp => resp.json())
-    },
-    async getAll() {
-        const resp = await fetch(`${remoteURL}/owners`);
-        return await resp.json();
-    }
+  getId(id) {
+    return fetch(`${remoteURL}/owners/${id}`).then(resp => resp.json());
+  },
+  async getAll() {
+    const resp = await fetch(`${remoteURL}/owners`);
+    return await resp.json();
+  }
 };
