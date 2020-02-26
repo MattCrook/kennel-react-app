5206f9aa3f8ad478c9c6d66a75127954cb30c6bb
Matt Crook <matt.crook11@gmail.com>
2020-02-26 09:39:29 -0600

    feat: delete button complete. Wip: details routing.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,15 +1,14 @@
 import { Route } from "react-router-dom";
 import React from "react";
 import Home from "./home/Home";
-import AnimalCard from "./animal/AnimalCard";
-import LocationCard from "./location/LocationCard";
-import EmployeeCard from "./employee/EmployeeCard";
-import OwnerCard from "./owner/OwnerCard";
-//import AnimalList from "./animal/AnimalList";
-
-// import LocationList from "./location/LocationList";
-// import EmployeeList from "./employee/EmployeeList";
-// import OwnerList from "./owner/OwnerList";
+// import AnimalCard from "./animal/AnimalCard";
+// import LocationCard from "./location/LocationCard";
+// import EmployeeCard from "./employee/EmployeeCard";
+// import OwnerCard from "./owner/OwnerCard";
+import AnimalList from "./animal/AnimalList";
+import LocationList from "./location/LocationList";
+import EmployeeList from "./employee/EmployeeList";
+import OwnerList from "./owner/OwnerList";
 
 const ApplicationViews = () => {
   return (
@@ -24,25 +23,25 @@ const ApplicationViews = () => {
       <Route
         path="/animals"
         render={props => {
-          return <AnimalCard />;
+          return <AnimalList {...props} />;
         }}
       />
       <Route
         path="/employees"
         render={props => {
-          return <EmployeeCard />;
+          return <EmployeeList {...props} />;
         }}
       />
       <Route
         path="/owners"
         render={props => {
-          return <OwnerCard />;
+          return <OwnerList {...props} />;
         }}
       />
       <Route
         path="/locations"
         render={props => {
-          return <LocationCard />;
+          return <LocationList {...props} />;
         }}
       />
     </React.Fragment>
